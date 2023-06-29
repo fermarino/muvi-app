f893d64b85e048a6988bc7e6acb89b9ba82cb6a2
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-29 16:17:11 -0300

    estrutura basica

---
+++ src/pages/Movies/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Movies() {
+  <View>
+    <Text>Filmes aqui</Text>
+  </View>
+}
+
+export default Movies;
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Movies from '../pages/Movies';
+import StackRoutes from './stackRoute';
+
+const Drawer = createDrawerNavigator();
+
+function Routes() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="HomeDrawer"
+        component={StackRoutes} />
+      <Drawer.Screen name="Movies" component={Movies} />
+    </Drawer.Navigator>
+  )
+}
+
+export default Routes;
