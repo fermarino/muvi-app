8f8f5affc0647cbcef275748849a96e38e59042e
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-29 17:22:57 -0300

    ajustando drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
 import Movies from '../pages/Movies';
 import StackRoutes from './stackRoute';
 
@@ -8,11 +10,48 @@ const Drawer = createDrawerNavigator();
 
 function Routes() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+
+        drawerStyle:{
+          backgroundColor: '#010D22',
+          paddingTop: 25,
+        },
+
+        drawerActiveBackgroundColor: '#142036',
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#fff'
+      }}
+    >
       <Drawer.Screen
         name="HomeDrawer"
-        component={StackRoutes} />
-      <Drawer.Screen name="Movies" component={Movies} />
+        component={StackRoutes}
+        options={{
+          title: 'Home',
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'movie-open' : 'movie-outline' }
+              size={size}
+              color={color}
+             
+            />
+          )
+        }} />
+      <Drawer.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        title: 'Meus filmes',
+        drawerIcon: ({ focused, size, color }) => (
+          <MaterialCommunityIcons
+            name={focused ? 'archive-star' : 'archive-outline'}
+            size={size}
+            color={color}
+          />
+        )
+      }}
+      />
     </Drawer.Navigator>
   )
 }
