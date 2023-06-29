ad49089e7225b979bfc55798a0e27569088ebc56
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-29 19:39:12 -0300

    add campo de busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container } from './styles'
+import { Container, SearchContainer, Input, SearchButton } from './styles';
+import { Feather } from '@expo/vector-icons';
 
-import Header from '../../components/Header'
+import Header from '../../components/Header';
 
 function Home() {
   return(
     <Container>
       <Header title="Muvi"/>
+
+      <SearchContainer>
+        <Input
+          placeholder="Pesquisar"
+          placeholderTextColor="#495057"
+        />
+        <SearchButton>
+          <Feather name="search" size={25} color="#fff" />
+        </SearchButton>
+      </SearchContainer>
+
     </Container>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView `
+export const Container = styled.SafeAreaView`
   background-color: #010D22;
   flex: 1;
   padding: 4px 0; 
 `;
+
+export const SearchContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 0 14px;
+  margin-bottom: 8px;
+  
+  height: 50px;
+  width: 100%;
+
+`;
+
+export const Input = styled.TextInput`
+  background-color: transparent;
+  border: 0.5px solid #fff;
+  border-radius: 10px;
+  padding: 8px 15px;
+  font-size: 20px;
+  color: #fff;
+
+  width: 85%;
+  height: 50px;
+`;
+
+export const SearchButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  
+  height: 50px;
+  width: 15%;
+`;
+
+
+
