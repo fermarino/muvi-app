4085a55fabe60beaedcbbda15d70ecf3bdeef701
fermarino <119690095+fermarino@users.noreply.github.com>
2023-06-30 18:15:40 -0300

    add banner e navegação na aba de detalhes

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import { Container, Title } from './styles';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  Header,
+  HeaderButton,
+  Banner
+} from './styles';
+
+import { Feather, Ionicons } from '@expo/vector-icons';
+
+import { useNavigation, useRoute } from '@react-navigation/native';
+import api, { key } from '../../services/api';
 
 function Details() {
-  return(
-    <Container>
-      <Title>Detalhes do filme aqui</Title>
-    </Container>
-  )
-};
+  const navigation = useNavigation();
+  const route = useRoute();
 
-export default Details; 
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getMovie() {
+      const response = await api.get(`/movie/${route.params?.id}`, {
+        params: {
+          api_key: key,
+          language: 'pt-BR', 
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+      if(isActive) {
+        setMovie(response.data)
+      }
+
+    }
+
+    if(isActive) {
+      getMovie();
+    }
+
+    return () => {
+      isActive = false
+    }
+
+  }, [])
+
+  return (
+    <Container>
+      <Header>
+        <HeaderButton activeOpacity={0.7} onPress={ () => navigation.goBack() } >
+          <Feather
+            name="arrow-left"
+            size={25}
+            color="#fff" />
+        </HeaderButton>
+        <HeaderButton activeOpacity={0.7}>
+          <Ionicons
+            name="bookmark"
+            size={25}
+            color="#fff" />
+        </HeaderButton>
+      </Header>
+
+      <Banner
+        resizeMethod='resize'
+        source={{ uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}` }}
+      />
+    </Container>
+  );
+}
+
+export default Details;
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,9 +1,38 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  background-color: #010D22;
+  flex: 1;
+`
+
+export const Header = styled.View`
+  position: absolute;
+  display: flex;
+  top: 30px;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 15px;
+  width: 100%;
+  z-index: 99;
+  
+`
+
+export const HeaderButton = styled.TouchableOpacity`
+  background-color: #010D99 ;
+  border-radius: 25px;
+  justify-content: center;
+  align-items: center;
+  width: 45px;
+  height: 45px;
+
 
 `
 
-export const Title = styled.Text`
-
+export const Banner = styled.Image`
+  border-bottom-left-radius: 45px;
+  border-bottom-right-radius: 45px;
+  object-fit: fill;
+  height: 400px;
+  width: 100%;
 `
+
